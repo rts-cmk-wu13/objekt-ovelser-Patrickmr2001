1a807ac279850d83e63aaf7efa9ce737158cbebe
Patrick <patrickmassesson@gmail.com>
2025-02-03 13:19:42 +0100

    opgave

---
+++ java.js
@@ -0,0 +1,10 @@
+let person1 = {
+    firstname: "Patrick",
+    lastname: "Rasmussen",
+    age: "23",
+    hometown: "Copenhagen",
+    pet: "Dog",
+    petname: "Alba"
+}
+
+console.log(person1.firstname + " " + person1.lastname + " " + person1.age + " " + person1.hometown + " " + person1.pet + " " + person1.petname);
